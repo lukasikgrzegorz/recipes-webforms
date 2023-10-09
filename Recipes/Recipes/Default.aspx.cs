ac044c61d4d5624aa4956d406ba069f0665a4280
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-10-09 21:58:22 +0200

    fix ingredients list/add photos

--- Recipes/Recipes/Default.aspx.cs
+++ Recipes/Recipes/Default.aspx.cs
@@ -35,35 +35,47 @@ public partial class _Default : System.Web.UI.Page
 
                         result.Text = "";
 
-                        foreach (Recipe recipe in recipes)
+                        if (recipes != null && recipes.Count > 0)
                         {
-                            // Tworzenie linka do przepisu
-                            HyperLink recipeHyperlink = new HyperLink();
-                            recipeHyperlink.NavigateUrl = "RecipePage.aspx?recipeId=" + recipe.IdMeal;
-                            recipeHyperlink.Text = recipe.StrMeal;
+                            foreach (Recipe recipe in recipes)
+                            {
+                                // Tworzenie linka do przepisu
+                                HyperLink recipeHyperlink = new HyperLink();
+                                recipeHyperlink.NavigateUrl = "RecipePage.aspx?recipeId=" + recipe.IdMeal;
+                                recipeHyperlink.Text = recipe.StrMeal;
+                                recipeHyperlink.Text += "<img src=\"" + (recipe.StrMealThumb ?? "") + "\" alt=\"" + (recipe.StrMeal ?? "") + "\" /><br/>";
 
-                            // Dodawanie linka do strony
-                            result.Controls.Add(recipeHyperlink);
+                                // Dodawanie linka do strony
+                                result.Controls.Add(recipeHyperlink);
 
-                            // Dodawanie separatora
-                            result.Controls.Add(new LiteralControl("<br/>"));
+                                // Dodawanie separatora
+                                result.Controls.Add(new LiteralControl("<br/>"));
+                            }
+                        }
+                        else
+                        {
+                            result.Text = "No results for: " + query;
                         }
                     }
                 }
             }
         }
+        catch (WebException webEx)
+        {
+            result.Text = "API Error: " + webEx.Message;
+        }
         catch (Exception ex)
         {
-            result.Text = "Wystąpił błąd: " + ex.Message;
+            result.Text = "Error: " + ex.Message;
         }
     }
-
 }
 
 public class Recipe
 {
     public string IdMeal { get; set; }
     public string StrMeal { get; set; }
+    public string StrMealThumb { get; set; }
 }
 
 public class MealApiResponse
--- Recipes/Recipes/RecipePage.aspx.cs
+++ Recipes/Recipes/RecipePage.aspx.cs
@@ -15,6 +15,10 @@ public partial class RecipePage : System.Web.UI.Page
                 string recipeId = Request.QueryString["recipeId"];
                 LoadRecipeDetails(recipeId);
             }
+            else
+            {
+                recipeDetailsLabel.Text = "No recipe identifier provided.";
+            }
         }
     }
 
@@ -22,7 +26,7 @@ public partial class RecipePage : System.Web.UI.Page
     {
         try
         {
-            // Tworzenie zapytania do API na podstawie recipeId
+            // Creating a request to the API based on recipeId
             WebRequest request = WebRequest.Create("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recipeId);
 
             using (WebResponse response = request.GetResponse())
@@ -33,36 +37,42 @@ public partial class RecipePage : System.Web.UI.Page
                     {
                         string apiResponse = reader.ReadToEnd();
 
-                        // Deserializacja odpowiedzi API do obiektu RecipeDetailsApiResponse
+                        // Deserializing API response to RecipeDetailsApiResponse object
                         RecipeDetailsApiResponse recipeDetailsApiResponse = JsonConvert.DeserializeObject<RecipeDetailsApiResponse>(apiResponse);
 
-                        // Pobranie pierwszego przepisu z odpowiedzi (zakładając, że recipeId jest unikalne)
-                        RecipeDetails recipeDetails = recipeDetailsApiResponse.Meals[0];
-
-                        // Wyświetlenie pełnego przepisu w kontrolce Label
-                        recipeDetailsLabel.Text = "Name: " + recipeDetails.StrMeal + "<br/>";
-                        recipeDetailsLabel.Text += "Category: " + recipeDetails.StrCategory + "<br/>";
-                        recipeDetailsLabel.Text += "Area: " + recipeDetails.StrArea + "<br/>";
-                        recipeDetailsLabel.Text += "Instructions: " + recipeDetails.StrInstructions + "<br/>";
-                        recipeDetailsLabel.Text += "Tags: " + recipeDetails.StrTags + "<br/>";
-                        recipeDetailsLabel.Text += "YouTube Video: <a href=\"" + recipeDetails.StrYoutube + "\" target=\"_blank\">" + recipeDetails.StrYoutube + "</a><br/>";
-                        recipeDetailsLabel.Text += "<img src=\"" + recipeDetails.StrMealThumb + "\" alt=\"" + recipeDetails.StrMeal + "\" /><br/>";
-
-                        // Sprawdź, czy Ingredients nie jest null
-                        if (recipeDetails.Ingredients != null && recipeDetails.Ingredients.Count > 0)
+                        if (recipeDetailsApiResponse.Meals != null && recipeDetailsApiResponse.Meals.Count > 0)
                         {
-                            // Wyświetl składniki
-                            recipeDetailsLabel.Text += "<h2>Ingredients:</h2><ul>";
-                            foreach (Ingredient ingredient in recipeDetails.Ingredients)
+                            RecipeDetails recipeDetails = recipeDetailsApiResponse.Meals[0];
+
+                            // Displaying the full recipe in a Label control
+                            recipeDetailsLabel.Text = "Name: " + (recipeDetails.StrMeal ?? "No name") + "<br/>";
+                            recipeDetailsLabel.Text += "Category: " + (recipeDetails.StrCategory ?? "No category") + "<br/>";
+                            recipeDetailsLabel.Text += "Area: " + (recipeDetails.StrArea ?? "No area") + "<br/>";
+                            recipeDetailsLabel.Text += "Instructions: " + (recipeDetails.StrInstructions ?? "No instructions") + "<br/>";
+                            recipeDetailsLabel.Text += "Tags: " + (recipeDetails.StrTags ?? "No tags") + "<br/>";
+                            recipeDetailsLabel.Text += "YouTube Video: <a href=\"" + (recipeDetails.StrYoutube ?? "#") + "\" target=\"_blank\">" + (recipeDetails.StrYoutube ?? "No YouTube video") + "</a><br/>";
+                            recipeDetailsLabel.Text += "<img src=\"" + (recipeDetails.StrMealThumb ?? "") + "\" alt=\"" + (recipeDetails.StrMeal ?? "") + "\" /><br/>";
+
+                            // Adding ingredients to a list
+                            recipeDetailsLabel.Text += "Ingredients:<ul>";
+                            for (int i = 1; i <= 20; i++)
                             {
-                                recipeDetailsLabel.Text += "<li>" + ingredient.Name + " - " + ingredient.Measure + "</li>";
+                                string ingredientProperty = "StrIngredient" + i;
+                                string ingredientValue = recipeDetails.GetType().GetProperty(ingredientProperty)?.GetValue(recipeDetails) as string;
+
+                                string measureProperty = "StrMeasure" + i;
+                                string measureValue = recipeDetails.GetType().GetProperty(measureProperty)?.GetValue(recipeDetails) as string;
+
+                                if (!string.IsNullOrEmpty(ingredientValue) && !string.IsNullOrEmpty(measureValue))
+                                {
+                                    recipeDetailsLabel.Text += "<li>" + ingredientValue + ":  " + measureValue + "</li>";
+                                }
                             }
                             recipeDetailsLabel.Text += "</ul>";
                         }
                         else
                         {
-                            // Ingredients jest null lub puste, obsłuż tę sytuację
-                            recipeDetailsLabel.Text += "<p>No ingredients information available.</p>";
+                            recipeDetailsLabel.Text = "No results for recipe with id: " + recipeId;
                         }
                     }
                 }
@@ -70,7 +80,7 @@ public partial class RecipePage : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            recipeDetailsLabel.Text = "Wystąpił błąd: " + ex.Message;
+            recipeDetailsLabel.Text = "An error occurred: " + ex.Message;
         }
     }
 }
@@ -90,13 +100,44 @@ public class RecipeDetails
     public string StrMealThumb { get; set; }
     public string StrTags { get; set; }
     public string StrYoutube { get; set; }
-
-    // Dodaj właściwość do przechowywania składników
-    public List<Ingredient> Ingredients { get; set; }
-}
-
-public class Ingredient
-{
-    public string Name { get; set; }
-    public string Measure { get; set; }
+    public string StrIngredient1 { get; set; }
+    public string StrIngredient2 { get; set; }
+    public string StrIngredient3 { get; set; }
+    public string StrIngredient4 { get; set; }
+    public string StrIngredient5 { get; set; }
+    public string StrIngredient6 { get; set; }
+    public string StrIngredient7 { get; set; }
+    public string StrIngredient8 { get; set; }
+    public string StrIngredient9 { get; set; }
+    public string StrIngredient10 { get; set; }
+    public string StrIngredient11 { get; set; }
+    public string StrIngredient12 { get; set; }
+    public string StrIngredient13 { get; set; }
+    public string StrIngredient14 { get; set; }
+    public string StrIngredient15 { get; set; }
+    public string StrIngredient16 { get; set; }
+    public string StrIngredient17 { get; set; }
+    public string StrIngredient18 { get; set; }
+    public string StrIngredient19 { get; set; }
+    public string StrIngredient20 { get; set; }
+    public string StrMeasure1 { get; set; }
+    public string StrMeasure2 { get; set; }
+    public string StrMeasure3 { get; set; }
+    public string StrMeasure4 { get; set; }
+    public string StrMeasure5 { get; set; }
+    public string StrMeasure6 { get; set; }
+    public string StrMeasure7 { get; set; }
+    public string StrMeasure8 { get; set; }
+    public string StrMeasure9 { get; set; }
+    public string StrMeasure10 { get; set; }
+    public string StrMeasure11 { get; set; }
+    public string StrMeasure12 { get; set; }
+    public string StrMeasure13 { get; set; }
+    public string StrMeasure14 { get; set; }
+    public string StrMeasure15 { get; set; }
+    public string StrMeasure16 { get; set; }
+    public string StrMeasure17 { get; set; }
+    public string StrMeasure18 { get; set; }
+    public string StrMeasure19 { get; set; }
+    public string StrMeasure20 { get; set; }
 }
